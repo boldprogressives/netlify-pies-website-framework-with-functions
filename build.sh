b3a425ae349597f517996035f189fdbbc1ab8405
Ethan Jucovy <ethan.jucovy@gmail.com>
2019-09-07 11:44:10 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 set -e
-mkdir -p ~/.ssh/
-cp id_rsa ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
-git clone --depth 1 git@github.com:boldprogressives/netlify-pies-website-framework-with-functions.git .
+chmod 600 id_rsa
+ssh-agent bash -c 'ssh-add id_rsa; git clone --depth 1 git@github.com:boldprogressives/netlify-pies-website-framework-with-functions.git' 
+rm -rf netlify-pies-website-framework-with-functions/.git
+cp -r netlify-pies-website-framework-with-functions/* ./
+rmdir netlify-pies-website-framework-with-functions
 netlify-lambda build functions
 rm -rf public && mkdir public 
 unzip bundle.zip -d public
