4fd0600806ce4c3e8797f4e441a9faa71a82547f
Ethan Jucovy <ethan.jucovy@gmail.com>
2019-09-07 11:50:57 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 set -e
-chmod 600 id_rsa
 rm -rf netlify-pies-website-framework-with-functions/
-ssh-agent bash -c 'ssh-add id_rsa; git clone --depth 1 git@github.com:boldprogressives/netlify-pies-website-framework-with-functions.git' 
+git clone --depth 1 git://github.com/boldprogressives/netlify-pies-website-framework-with-functions.git
 rm -rf netlify-pies-website-framework-with-functions/.git
 cp -r netlify-pies-website-framework-with-functions/* ./
 rm -rf netlify-pies-website-framework-with-functions/
