bf70011ebd5e78ab3dc8e2f53579bd7458662c54
Ethan Jucovy <ethan.jucovy@gmail.com>
2019-09-07 11:27:53 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 set -e
 mkdir -p ~/.ssh/
 cp id_rsa ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
-git clone --depth 1 git@github.com/boldprogressives/netlify-pies-website-framework-with-functions.git .
+git clone --depth 1 git@github.com:boldprogressives/netlify-pies-website-framework-with-functions.git .
 netlify-lambda build functions
 rm -rf public && mkdir public 
 unzip bundle.zip -d public
