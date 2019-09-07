5337d4af53ec2f948eaddd76ae584b5e98a22a9a
Ethan Jucovy <ethan.jucovy@gmail.com>
2019-09-07 10:46:23 -0400

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+mkdir -p ~/.ssh/
+cp id_rsa ~/.ssh/id_rsa && chmod 600 ~/.ssh/id_rsa
+git clone --depth 1 git@github.com/boldprogressives/netlify-pies-website-framework-with-functions.git .
+netlify-lambda build functions
+rm -rf public && mkdir public 
+unzip bundle.zip -d public
