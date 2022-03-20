646c27552a4faef9c0624fbeb9dac0b3cd061aef
Ethan Jucovy <ethan.jucovy@gmail.com>
2022-03-19 22:16:49 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 rm -rf netlify-pies-website-framework-with-functions/
-git clone --depth 1 git://github.com/boldprogressives/netlify-pies-website-framework-with-functions.git
+git clone --depth 1 https://github.com/boldprogressives/netlify-pies-website-framework-with-functions
 rm -rf netlify-pies-website-framework-with-functions/.git
 cp -r netlify-pies-website-framework-with-functions/* ./
-rm -rf netlify-pies-website-framework-with-functions/
+rm -rf netlify-pies-website-framework-with-functions
 netlify-lambda build functions
 rm -rf public && mkdir public 
 unzip -o bundle.zip -d public
